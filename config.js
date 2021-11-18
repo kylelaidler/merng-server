0be0394c5b9498fa9d9ba66a62eb93094bb752b2
Kyle Laidler <aliciaklassen@Alicias-MacBook-Pro.local>
2021-11-18 12:44:42 +0200

    Init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: 
+        'mongodb+srv://classed:Poofies123@cluster0.dg7pm.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+};
